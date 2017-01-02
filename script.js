e041418ef51ba4163b44f7feff16031f0351b8d3
juanpfc <juanscript@gmail.com>
2017-01-02 17:11:16 -0200

    versão inicial angular (estudos)
    versão inicial angular (estudos)

---
+++ script.js
@@ -0,0 +1,33 @@
+// Code goes here
+
+(function() {
+  var app = angular.module("githubViewer", []);
+
+  var MainController = function($scope, $http) {
+    
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the user";
+    };
+
+    $scope.search = function(username) {
+      $http.get("https://api.github.com/users/" + username)
+        .then(onUserComplete, onError);
+
+      //$http.get("https://api.github.com/users/angular")
+      //.then(onUserComplete,onError);
+    };
+
+    $scope.username = "angular";
+    $scope.message = "GitHub Viewer";
+
+  };
+
+  app.controller("MainController", ["$scope", "$http", MainController]);
+
+
+
+}());
